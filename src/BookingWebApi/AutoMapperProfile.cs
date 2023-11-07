0a674f2af7d8ab705905abd2039f750114e3e0a8
Marlon Couto <coutoozair@gmail.com>
2023-11-07 18:10:08 -0300

    feat: update AutoMapperProfile.cs

--- src/BookingWebApi/AutoMapperProfile.cs
+++ src/BookingWebApi/AutoMapperProfile.cs
@@ -12,6 +12,10 @@ namespace BookingWebApi
             CreateMap<Booking, BookingDto>()
                 .ForMember(dest => dest.User, opts => opts.MapFrom(src => src.User!.Name))
                 .ForMember(dest => dest.Room, opts => opts.MapFrom(src => src.Room!.Name));
+
+            CreateMap<User, UserDto>()
+                .ForMember(dest => dest.Bookings, opts => opts.MapFrom(src => src.Bookings!.ToList()
+                    .Select(booking => booking.BookingId)));
         }
     }
 }
