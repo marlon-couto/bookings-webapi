3a27896e6d01102d7deea96ee2ad916f24b3ee71
Marlon Couto <coutoozair@gmail.com>
2023-11-07 18:08:59 -0300

    feat: add UserController.cs

---
+++ src/BookingWebApi/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+using AutoMapper;
+
+using BookingWebApi.Data;
+using BookingWebApi.Dtos;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookingWebApi.Controllers
+{
+    [ApiController]
+    [Route("user")]
+    public class UserController : Controller
+    {
+        private readonly IBookingWebApiContext _context;
+        private readonly IMapper _mapper;
+        public UserController(IBookingWebApiContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult FindAllUsersBookings()
+        {
+            var users = _context.Users
+                .Include(user => user.Bookings);
+
+            var mappedUsers = users
+                .Select(user => _mapper.Map<UserDto>(user));
+
+            return Ok(mappedUsers);
+        }
+    }
+}
