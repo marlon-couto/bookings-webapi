43cdb495b9c1c6569c7ae55030ea023e42829d5c
Marlon Couto <coutoozair@gmail.com>
2023-11-07 18:09:35 -0300

    feat: update BookingDto.cs

--- src/BookingWebApi/Dtos/BookingDto.cs
+++ src/BookingWebApi/Dtos/BookingDto.cs
@@ -6,9 +6,7 @@ namespace BookingWebApi.Dtos
         public DateTime CheckIn { get; set; }
         public DateTime CheckOut { get; set; }
         public int GuestQuantity { get; set; }
-        public string UserId { get; set; } = string.Empty;
         public string User { get; set; } = string.Empty;
-        public string RoomId { get; set; } = string.Empty;
         public string Room { get; set; } = string.Empty;
     }
 }
