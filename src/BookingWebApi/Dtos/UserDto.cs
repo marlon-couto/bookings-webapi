969127c3352e9da060cc280aaf8856dcf6fdbea0
Marlon Couto <coutoozair@gmail.com>
2023-11-08 15:55:21 -0300

    feat: update UserDto.cs

--- src/BookingWebApi/Dtos/UserDto.cs
+++ src/BookingWebApi/Dtos/UserDto.cs
@@ -6,6 +6,5 @@ namespace BookingWebApi.Dtos
         public string Name { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Role { get; set; } = string.Empty;
-        public virtual List<string>? Bookings { get; set; }
     }
 }
