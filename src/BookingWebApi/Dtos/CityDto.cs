909d2aa178a98c08f2a4076a2836419c53215927
Marlon Couto <coutoozair@gmail.com>
2023-11-08 15:55:29 -0300

    feat: add CityDto.cs

---
+++ src/BookingWebApi/Dtos/CityDto.cs
@@ -0,0 +1,9 @@
+namespace BookingWebApi.Dtos
+{
+    public class CityDto
+    {
+        public string CityId { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+    }
+}
