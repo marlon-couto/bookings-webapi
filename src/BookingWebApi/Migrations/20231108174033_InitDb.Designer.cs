7d5cba6af2c9d01bd6156183148ffac39566a9f6
Marlon Couto <coutoozair@gmail.com>
2023-11-08 15:56:01 -0300

    feat: update migrations

--- src/BookingWebApi/Migrations/20231108174033_InitDb.Designer.cs
+++ src/BookingWebApi/Migrations/20231108174033_InitDb.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BookingWebApi.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BookingWebApi.Migrations
 {
     [DbContext(typeof(BookingWebApiContext))]
-    partial class BookingWebApiContextModelSnapshot : ModelSnapshot
+    [Migration("20231108174033_InitDb")]
+    partial class InitDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.13");
--- src/BookingWebApi/Migrations/20231108174033_InitDb.cs
+++ src/BookingWebApi/Migrations/20231108174033_InitDb.cs
@@ -3,6 +3,8 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
 namespace BookingWebApi.Migrations
 {
     /// <inheritdoc />
@@ -25,6 +27,21 @@ namespace BookingWebApi.Migrations
                 });
 
             migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    Password = table.Column<string>(type: "TEXT", nullable: false),
+                    Role = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
                 name: "Hotels",
                 columns: table => new
                 {
@@ -36,6 +53,12 @@ namespace BookingWebApi.Migrations
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_Hotels", x => x.HotelId);
+                    table.ForeignKey(
+                        name: "FK_Hotels_Cities_CityId",
+                        column: x => x.CityId,
+                        principalTable: "Cities",
+                        principalColumn: "CityId",
+                        onDelete: ReferentialAction.Cascade);
                 });
 
             migrationBuilder.CreateTable(
@@ -51,21 +74,12 @@ namespace BookingWebApi.Migrations
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_Rooms", x => x.RoomId);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "Users",
-                columns: table => new
-                {
-                    UserId = table.Column<string>(type: "TEXT", nullable: false),
-                    Name = table.Column<string>(type: "TEXT", nullable: false),
-                    Email = table.Column<string>(type: "TEXT", nullable: false),
-                    Password = table.Column<string>(type: "TEXT", nullable: false),
-                    Role = table.Column<string>(type: "TEXT", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Users", x => x.UserId);
+                    table.ForeignKey(
+                        name: "FK_Rooms_Hotels_HotelId",
+                        column: x => x.HotelId,
+                        principalTable: "Hotels",
+                        principalColumn: "HotelId",
+                        onDelete: ReferentialAction.Cascade);
                 });
 
             migrationBuilder.CreateTable(
@@ -97,19 +111,56 @@ namespace BookingWebApi.Migrations
                 });
 
             migrationBuilder.InsertData(
-                table: "Rooms",
-                columns: new[] { "RoomId", "Capacity", "HotelId", "Image", "Name" },
-                values: new object[] { "1", 2, "1", "Image 1", "Room 1" });
+                table: "Cities",
+                columns: new[] { "CityId", "Name", "State" },
+                values: new object[,]
+                {
+                    { "1", "City 1", "State 1" },
+                    { "2", "City 2", "State 2" }
+                });
 
             migrationBuilder.InsertData(
                 table: "Users",
                 columns: new[] { "UserId", "Email", "Name", "Password", "Role" },
-                values: new object[] { "1", "user1@mail.com", "User 1", "Pass1", "Client" });
+                values: new object[,]
+                {
+                    { "1", "user1@mail.com", "User 1", "Pass1", "Client" },
+                    { "2", "user2@mail.com", "User 2", "Pass2", "Client" },
+                    { "3", "user3@mail.com", "User 3", "Pass3", "Admin" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "HotelId", "Address", "CityId", "Name" },
+                values: new object[,]
+                {
+                    { "1", "Address 1", "1", "Hotel 1" },
+                    { "2", "Address 2", "2", "Hotel 2" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Rooms",
+                columns: new[] { "RoomId", "Capacity", "HotelId", "Image", "Name" },
+                values: new object[,]
+                {
+                    { "1", 2, "1", "Image 1", "Room 1" },
+                    { "2", 1, "1", "Image 2", "Room 2" },
+                    { "3", 3, "1", "Image 3", "Room 3" },
+                    { "4", 2, "2", "Image 4", "Room 4" },
+                    { "5", 1, "2", "Image 5", "Room 5" },
+                    { "6", 3, "2", "Image 6", "Room 6" }
+                });
 
             migrationBuilder.InsertData(
                 table: "Bookings",
                 columns: new[] { "BookingId", "CheckIn", "CheckOut", "GuestQuantity", "RoomId", "UserId" },
-                values: new object[] { "1", new DateTime(2023, 11, 7, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 11, 8, 0, 0, 0, 0, DateTimeKind.Unspecified), 2, "1", "1" });
+                values: new object[,]
+                {
+                    { "1", new DateTime(2023, 11, 7, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 11, 8, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, "1", "1" },
+                    { "2", new DateTime(2023, 11, 7, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 11, 8, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, "2", "1" },
+                    { "3", new DateTime(2023, 11, 7, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 11, 8, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, "3", "2" },
+                    { "4", new DateTime(2023, 11, 7, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 11, 8, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, "4", "2" }
+                });
 
             migrationBuilder.CreateIndex(
                 name: "IX_Bookings_RoomId",
@@ -120,6 +171,16 @@ namespace BookingWebApi.Migrations
                 name: "IX_Bookings_UserId",
                 table: "Bookings",
                 column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Hotels_CityId",
+                table: "Hotels",
+                column: "CityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rooms_HotelId",
+                table: "Rooms",
+                column: "HotelId");
         }
 
         /// <inheritdoc />
@@ -129,16 +190,16 @@ namespace BookingWebApi.Migrations
                 name: "Bookings");
 
             migrationBuilder.DropTable(
-                name: "Cities");
+                name: "Rooms");
+
+            migrationBuilder.DropTable(
+                name: "Users");
 
             migrationBuilder.DropTable(
                 name: "Hotels");
 
             migrationBuilder.DropTable(
-                name: "Rooms");
-
-            migrationBuilder.DropTable(
-                name: "Users");
+                name: "Cities");
         }
     }
 }
