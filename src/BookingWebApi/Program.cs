a4022a03c7117092cb2980f2859676ab57362a08
Marlon Couto <coutoozair@gmail.com>
2023-11-14 00:42:49 -0300

    feat: update Program.cs

--- src/BookingWebApi/Program.cs
+++ src/BookingWebApi/Program.cs
@@ -1,13 +1,18 @@
+using BookingWebApi.Dtos;
 using BookingWebApi.Repositories;
+
+using FluentValidation;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<BookingWebApiContext>(); // permite que o contexto seja usado pelo EF Core
-// adiciona escopos para a injeção de dependência
+builder.Services.AddDbContext<BookingWebApiContext>(); // Permite que o contexto seja usado pelo EF Core.
+// Adiciona escopos para a injeção de dependência.
 builder.Services.AddScoped<IBookingWebApiContext, BookingWebApiContext>();
+// Validações de corpo da requisição
+builder.Services.AddScoped<IValidator<BookingInsertDto>, BookingInsertValidator>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
