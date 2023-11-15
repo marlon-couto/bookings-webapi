df3d42365cd793cf1f2d17e3e0f567a70856c68d
Marlon Couto <coutoozair@gmail.com>
2023-11-15 16:59:46 -0300

    feat: add HotelRepository

---
+++ src/BookingsWebApi/Repositories/HotelRepository.cs
@@ -0,0 +1,43 @@
+using AutoMapper;
+
+using BookingsWebApi.Dtos;
+using BookingsWebApi.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace BookingsWebApi.Repositories
+{
+    public class HotelRepository : IHotelRepository
+    {
+        private readonly IBookingsDbContext _context;
+        private readonly IMapper _mapper;
+        public HotelRepository(IBookingsDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<List<HotelDto>> GetAllHotels()
+        {
+            List<Hotel> allHotels = await _context.Hotels.Include(h => h.City).ToListAsync();
+            return allHotels.Select(h => _mapper.Map<HotelDto>(h)).ToList();
+        }
+
+        public async Task<City?> GetCityById(string cityId)
+        {
+            return await _context.Cities.FirstOrDefaultAsync(c => c.CityId == cityId);
+        }
+
+        public async Task<HotelDto> AddHotel(HotelInsertDto hotelInsert, City cityFound)
+        {
+            Hotel newHotel = _mapper.Map<Hotel>(hotelInsert);
+            newHotel.HotelId = Guid.NewGuid().ToString();
+
+            await _context.Hotels.AddAsync(newHotel);
+            _context.SaveChanges(); // TODO: mudar essa implementação para um método assíncrono.
+
+            newHotel.City = cityFound;
+            return _mapper.Map<HotelDto>(newHotel);
+        }
+    }
+}
