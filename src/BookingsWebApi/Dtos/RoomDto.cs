5635732aef394049c8403edde34dd4a7720eee41
Marlon Couto <coutoozair@gmail.com>
2023-11-15 16:59:23 -0300

    feat: update RoomDto

--- src/BookingsWebApi/Dtos/RoomDto.cs
+++ src/BookingsWebApi/Dtos/RoomDto.cs
@@ -8,4 +8,12 @@ namespace BookingsWebApi.Dtos
         public string Image { get; set; } = string.Empty;
         public HotelDto? Hotel { get; set; }
     }
+
+    public class RoomInsertDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public int Capacity { get; set; }
+        public string Image { get; set; } = string.Empty;
+        public string HotelId { get; set; } = string.Empty;
+    }
 }
