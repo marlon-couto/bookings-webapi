bf6ceb5a38182837e9b09664e723a131c5ad0afe
Marlon Couto <coutoozair@gmail.com>
2023-11-15 14:16:48 -0300

    feat: format UserValidator

--- src/BookingsWebApi/Dtos/UserValidator.cs
+++ src/BookingsWebApi/Dtos/UserValidator.cs
@@ -6,9 +6,9 @@ namespace BookingsWebApi.Dtos
     {
         public UserInsertValidator()
         {
-            RuleFor(b => b.Name).NotEmpty().MinimumLength(2);
-            RuleFor(b => b.Email).NotEmpty().EmailAddress();
-            RuleFor(b => b.Password)
+            RuleFor(u => u.Name).NotEmpty().MinimumLength(2);
+            RuleFor(u => u.Email).NotEmpty().EmailAddress();
+            RuleFor(u => u.Password)
                 .NotEmpty()
                 .Matches("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$")
                 .WithMessage("'Password' must be at least 8 character long, must include an lowercase letter and a uppercase letter, must have at least one number and one special character (#?!@$%^&*-)");
