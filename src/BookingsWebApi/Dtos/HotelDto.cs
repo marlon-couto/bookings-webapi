ce997e86f96f718e35a2aae916715273ceee3a47
Marlon Couto <coutoozair@gmail.com>
2023-11-15 16:58:45 -0300

    feat: update HotelDto

--- src/BookingsWebApi/Dtos/HotelDto.cs
+++ src/BookingsWebApi/Dtos/HotelDto.cs
@@ -9,4 +9,11 @@ namespace BookingsWebApi.Dtos
         public string CityName { get; set; } = string.Empty;
         public string CityState { get; set; } = string.Empty;
     }
+
+    public class HotelInsertDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string CityId { get; set; } = string.Empty;
+    }
 }
