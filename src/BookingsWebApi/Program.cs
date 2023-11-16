fd299d258e391ebdb8fb5025c743529a1999111f
Marlon Couto <coutoozair@gmail.com>
2023-11-16 13:48:27 -0300

    feat: update Program.cs

--- src/BookingsWebApi/Program.cs
+++ src/BookingsWebApi/Program.cs
@@ -1,7 +1,14 @@
+using System.Security.Claims;
+using System.Text;
+
 using BookingsWebApi.Dtos;
 using BookingsWebApi.Repositories;
+using BookingsWebApi.Services;
 
 using FluentValidation;
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,11 +32,40 @@ builder.Services.AddScoped<IValidator<UserInsertDto>, UserInsertValidator>();
 builder.Services.AddScoped<IValidator<CityInsertDto>, CityInsertValidator>();
 builder.Services.AddScoped<IValidator<HotelInsertDto>, HotelInsertValidator>();
 builder.Services.AddScoped<IValidator<RoomInsertDto>, RoomInsertValidator>();
+builder.Services.AddScoped<IValidator<LoginInsertDto>, LoginInsertValidator>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(typeof(Program).Assembly); // Configura o AutoMapper na aplicação.
+
+// Configura o JWT.
+builder.Services.AddScoped<ITokenGenerator, TokenGenerator>();
+
+var tokenOptions = builder.Configuration.GetSection(TokenOptions.Token);
+builder.Services.AddAuthentication(opts =>
+{
+    opts.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    opts.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(opts =>
+{
+    opts.SaveToken = true;
+    opts.RequireHttpsMetadata = false;
+    opts.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(tokenOptions.GetValue<string>("Secret")))
+    };
+});
+
+// Adiciona políticas de autorização à aplicação.
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("Client", policy => policy.RequireClaim(ClaimTypes.Email));
+    options.AddPolicy("Admin", policy => policy.RequireClaim(ClaimTypes.Email).RequireClaim(ClaimTypes.Role, "Admin"));
+});
 
 var app = builder.Build();
 
@@ -41,6 +77,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication(); // Habilita o middleware de autenticação.
 
 app.UseAuthorization();
 
