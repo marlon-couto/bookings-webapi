af6d92063111ed8414e47cafea24d5324794309c
Marlon Couto <coutoozair@gmail.com>
2023-11-17 14:38:19 -0300

    feat: update BookingRepository

--- src/BookingsWebApi/Repositories/BookingRepository.cs
+++ src/BookingsWebApi/Repositories/BookingRepository.cs
@@ -7,31 +7,16 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BookingsWebApi.Repositories
 {
-    /// <summary>
-    /// Repository for managing bookings in the application.
-    /// </summary>
     public class BookingRepository : IBookingRepository
     {
         private readonly IBookingsDbContext _context;
         private readonly IMapper _mapper;
-        /// <summary>
-        /// Initializes a new instance of the <see cref="BookingRepository"/> class.
-        /// </summary>
-        /// <param name="context">The database context for the application.</param>
-        /// <param name="mapper">The AutoMapper instance for object mapping.</param>
         public BookingRepository(IBookingsDbContext context, IMapper mapper)
         {
             _context = context;
             _mapper = mapper;
         }
 
-        /// <summary>
-        /// Adds a new booking to the database.
-        /// </summary>
-        /// <param name="inputData">The data to create a new booking.</param>
-        /// <param name="userFound">The user associated with the booking.</param>
-        /// <param name="roomFound">The room associated with the booking.</param>
-        /// <returns>A <see cref="BookingDto"/> representing the newly created booking.</returns>
         public async Task<BookingDto> AddBooking(BookingInsertDto inputData, User userFound, Room roomFound)
         {
             Booking newBooking = _mapper.Map<Booking>(inputData);
@@ -46,15 +31,6 @@ namespace BookingsWebApi.Repositories
             return _mapper.Map<BookingDto>(newBooking);
         }
 
-        /// <summary>
-        /// Retrieves a booking with the given ID from the database.
-        /// </summary>
-        /// <param name="id">The booking ID to search the database.</param>
-        /// <param name="userEmail">The email from the user associated with the booking.</param>
-        /// <returns>A <see cref="BookingDto"/> representing the booking found. </returns>
-        /// <exception cref="KeyNotFoundException">
-        /// Thrown if a booking with the given ID and email does not exist.
-        /// </exception>
         public async Task<BookingDto> GetBookingById(string id, string userEmail)
         {
             Booking? bookingFound = await _context.Bookings
@@ -69,26 +45,12 @@ namespace BookingsWebApi.Repositories
                 : _mapper.Map<BookingDto>(bookingFound);
         }
 
-        /// <summary>
-        /// Retrieves a room with the given ID from the database.
-        /// </summary>
-        /// <param name="roomId">The room ID to search the database.</param>
-        /// <returns>The <see cref="Room"/> found.</returns>
-        /// <exception cref="KeyNotFoundException">Thrown if a room with the given ID does not exist.</exception>
         public async Task<Room> GetRoomById(string roomId)
         {
             return await _context.Rooms.FirstOrDefaultAsync(r => r.RoomId == roomId)
                 ?? throw new KeyNotFoundException("The room with the id provided does not exist");
         }
 
-        /// <summary>
-        /// Retrieves a user with the given email from the database.
-        /// </summary>
-        /// <param name="userEmail">The email to search the database.</param>
-        /// <returns>The <see cref="User"/> found.</returns>
-        /// <exception cref="UnauthorizedAccessException">
-        /// Thrown if a user with the given email does not exist.
-        /// </exception>
         public async Task<User> GetUserByEmail(string userEmail)
         {
             return await _context.Users.FirstOrDefaultAsync(u => u.Email == userEmail)
--- src/BookingsWebApi/Repositories/IBookingRepository.cs
+++ src/BookingsWebApi/Repositories/IBookingRepository.cs
@@ -3,11 +3,47 @@ using BookingsWebApi.Models;
 
 namespace BookingsWebApi.Repositories
 {
+    /// <summary>
+    /// Repository for managing bookings in the application.
+    /// </summary>
     public interface IBookingRepository
     {
+        /// <summary>
+        /// Adds a new booking to the database.
+        /// </summary>
+        /// <param name="inputData">The data to create a new booking.</param>
+        /// <param name="userFound">The user associated with the booking.</param>
+        /// <param name="roomFound">The room associated with the booking.</param>
+        /// <returns>A <see cref="BookingDto"/> representing the newly created booking.</returns>
         public Task<BookingDto> AddBooking(BookingInsertDto inputData, User userFound, Room roomFound);
+
+        /// <summary>
+        /// Retrieves a booking with the given ID from the database.
+        /// </summary>
+        /// <param name="id">The booking ID to search the database.</param>
+        /// <param name="userEmail">The email from the user associated with the booking.</param>
+        /// <returns>A <see cref="BookingDto"/> representing the booking found. </returns>
+        /// <exception cref="KeyNotFoundException">
+        /// Thrown if a booking with the given ID and email does not exist.
+        /// </exception>
         public Task<BookingDto> GetBookingById(string id, string userEmail);
-        public Task<Room> GetRoomById(string id);
+
+        /// <summary>
+        /// Retrieves a room with the given ID from the database.
+        /// </summary>
+        /// <param name="roomId">The room ID to search the database.</param>
+        /// <returns>The <see cref="Room"/> found.</returns>
+        /// <exception cref="KeyNotFoundException">Thrown if a room with the given ID does not exist.</exception>
+        public Task<Room> GetRoomById(string roomId);
+
+        /// <summary>
+        /// Retrieves a user with the given email from the database.
+        /// </summary>
+        /// <param name="userEmail">The email to search the database.</param>
+        /// <returns>The <see cref="User"/> found.</returns>
+        /// <exception cref="UnauthorizedAccessException">
+        /// Thrown if a user with the given email does not exist.
+        /// </exception>
         public Task<User> GetUserByEmail(string userEmail);
     }
 }
