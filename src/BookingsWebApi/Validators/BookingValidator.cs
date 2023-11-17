563111acae5390efd205c10598d648c10251d40a
Marlon Couto <coutoozair@gmail.com>
2023-11-17 00:53:01 -0300

    feat: update Validators

--- src/BookingsWebApi/Validators/BookingValidator.cs
+++ src/BookingsWebApi/Validators/BookingValidator.cs
@@ -9,8 +9,8 @@ namespace BookingsWebApi.Validators
         public BookingInsertValidator()
         {
             RuleFor(b => b.GuestQuantity).NotEmpty().GreaterThan(0);
-            RuleFor(b => b.CheckIn).NotEmpty().Must(IsValidDate).WithMessage("'Check In' must be a valid date");
-            RuleFor(b => b.CheckOut).NotEmpty().Must(IsValidDate).WithMessage("'Check Out' must be a valid date");
+            RuleFor(b => b.CheckIn).NotEmpty().Must(IsValidDate).WithMessage("'Check In' must be a valid date.");
+            RuleFor(b => b.CheckOut).NotEmpty().Must(IsValidDate).WithMessage("'Check Out' must be a valid date.");
             RuleFor(b => b.RoomId).NotEmpty().MinimumLength(1); // TODO: modificar essa implementação.
         }
 
