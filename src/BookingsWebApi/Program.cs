5abd2aa0a9de3bc26dc958e83293dc3e07221586
Marlon Couto <coutoozair@gmail.com>
2023-11-17 14:38:55 -0300

    feat: update Program.cs

--- src/BookingsWebApi/Program.cs
+++ src/BookingsWebApi/Program.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using System.Security.Claims;
 using System.Text;
 
@@ -10,15 +11,16 @@ using FluentValidation;
 
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<BookingsDbContext>(); // Permite que o contexto seja usado pelo EF Core.
+builder.Services.AddDbContext<BookingsDbContext>(); // Allows the context to be used by EF Core.
 
-// Adiciona escopos para a injeção de dependência.
+// Adds scopes for dependency injection.
 builder.Services.AddScoped<IBookingsDbContext, BookingsDbContext>();
 builder.Services.AddScoped<IBookingRepository, BookingRepository>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
@@ -26,8 +28,8 @@ builder.Services.AddScoped<ICityRepository, CityRepository>();
 builder.Services.AddScoped<IHotelRepository, HotelRepository>();
 builder.Services.AddScoped<IRoomRepository, RoomRepository>();
 
-// Validações de corpo da requisição.
-ValidatorOptions.Global.LanguageManager.Enabled = false; // Desabilita a tradução das mensagens de erro.
+// Request body validations.
+ValidatorOptions.Global.LanguageManager.Enabled = false; // Disables the translation of error messages.
 builder.Services.AddScoped<IValidator<BookingInsertDto>, BookingInsertValidator>();
 builder.Services.AddScoped<IValidator<UserInsertDto>, UserInsertValidator>();
 builder.Services.AddScoped<IValidator<CityInsertDto>, CityInsertValidator>();
@@ -37,10 +39,22 @@ builder.Services.AddScoped<IValidator<LoginInsertDto>, LoginInsertValidator>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddAutoMapper(typeof(Program).Assembly); // Configura o AutoMapper na aplicação.
+builder.Services.AddSwaggerGen(opts =>
+{
+    opts.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Bookings.net API",
+        Description = "An API for managing bookings, rooms and hotels."
+    });
+    // Reflection is used to build an XML file name matching that of the web API project.
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    opts.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 
-// Configura o JWT.
+builder.Services.AddAutoMapper(typeof(Program).Assembly); // Configures AutoMapper in the application.
+
+// Configures JWT.
 builder.Services.AddScoped<ITokenGenerator, TokenGenerator>();
 
 var tokenOptions = builder.Configuration.GetSection(TokenOptions.Token);
@@ -61,7 +75,7 @@ builder.Services.AddAuthentication(opts =>
     };
 });
 
-// Adiciona políticas de autorização à aplicação.
+// Adds authorization policies to the application.
 builder.Services.AddAuthorization(options =>
 {
     options.AddPolicy("Client", policy => policy.RequireClaim(ClaimTypes.Email));
@@ -79,7 +93,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthentication(); // Habilita o middleware de autenticação.
+app.UseAuthentication(); // Enables authentication middleware.
 
 app.UseAuthorization();
 
