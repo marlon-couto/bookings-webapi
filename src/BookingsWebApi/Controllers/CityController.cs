7af2a66434617e80ff2a7e048113a2902dc31fc5
Marlon Couto <coutoozair@gmail.com>
2023-11-18 13:30:43 -0300

    feat: update CityController

--- src/BookingsWebApi/Controllers/CityController.cs
+++ src/BookingsWebApi/Controllers/CityController.cs
@@ -4,12 +4,15 @@ using BookingsWebApi.Repositories;
 
 using FluentValidation;
 
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookingsWebApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Produces("application/json")]
+    [Authorize(Policy = "Admin")]
     public class CityController : Controller
     {
         private readonly ICityRepository _repository;
@@ -20,7 +23,14 @@ namespace BookingsWebApi.Controllers
             _validator = validator;
         }
 
-        // Admin
+        /// <summary>
+        /// Deletes a city with the given ID.
+        /// </summary>
+        /// <param name="id">The ID of the city to delete.</param>
+        /// <returns>A status code 204 and no content.</returns>
+        /// <response code="204">Returns 204 with no content.</response>
+        /// <response code="401">If the user is unauthorized, returns 401.</response>
+        /// <response code="404">If the city is not found, returns 404 and an error message.</response>
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteAsync(string id)
         {
@@ -36,14 +46,37 @@ namespace BookingsWebApi.Controllers
             }
         }
 
+        /// <summary>
+        /// Retrieves all cities information.
+        /// </summary>
+        /// <returns>A JSON response representing the result of the operation</returns>
+        /// <response code="200">Returns 200 and the city data.</response>
         [HttpGet]
+        [AllowAnonymous]
         public async Task<IActionResult> GetAsync()
         {
             List<CityDto> allCities = await _repository.GetAllCities();
             return Ok(new { Data = allCities, Result = "Success" });
         }
 
-        // Admin
+        /// <summary>
+        /// Creates a new city based on the provided data.
+        /// </summary>
+        /// <param name="inputData">The data for creating a new city.</param>
+        /// <returns>A JSON response representing the result of the operation.</returns>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     POST /City
+        ///     {
+        ///         "name": "New City",
+        ///         "state": "State 1"
+        ///     }
+        ///
+        /// </remarks>
+        /// <response code="201">Returns 201 and the newly created city data.</response>
+        /// <response code="401">If the user is unauthorized, returns 401.</response>
+        /// <response code="400">If the input data is invalid, returns 400 and an error message.</response>
         [HttpPost]
         public async Task<IActionResult> PostAsync(CityInsertDto inputData)
         {
@@ -60,7 +93,26 @@ namespace BookingsWebApi.Controllers
             }
         }
 
-        // Admin
+        /// <summary>
+        /// Updates the city with the given ID based on the provided data.
+        /// </summary>
+        /// <param name="inputData">The data for updating the city retrieved.</param>
+        /// <param name="id">The ID of the city to update.</param>
+        /// <returns>A JSON response representing the result of the operation.</returns>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     PUT /City/1
+        ///     {
+        ///         "name": "New City",
+        ///         "state": "State 1"
+        ///     }
+        ///
+        /// </remarks>
+        /// <response code="200">Returns 201 and the updated city data.</response>
+        /// <response code="401">If the user is unauthorized, returns 401.</response>
+        /// <response code="400">If the input data is invalid, returns 400 and an error message.</response>
+        /// <response code="404">If a city with the provided ID not exists, returns 404 and an error message.</response>
         [HttpPut("{id}")]
         public async Task<IActionResult> PutAsync([FromBody] CityInsertDto inputData, string id)
         {
