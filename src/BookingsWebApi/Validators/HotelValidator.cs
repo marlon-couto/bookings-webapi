f7f5b684a463e7850b4920022fa5e92879c6be07
Marlon Couto <coutoozair@gmail.com>
2023-11-18 23:59:18 -0300

    feat: format Validators

--- src/BookingsWebApi/Validators/HotelValidator.cs
+++ src/BookingsWebApi/Validators/HotelValidator.cs
@@ -8,8 +8,8 @@ namespace BookingsWebApi.Validators
     {
         public HotelInsertValidator()
         {
-            RuleFor(h => h.Address).NotEmpty().Length(5, 50);
             RuleFor(h => h.Name).NotEmpty().Length(2, 25);
+            RuleFor(h => h.Address).NotEmpty().Length(5, 50);
             RuleFor(h => h.CityId).NotEmpty().MinimumLength(1);
         }
     }
--- src/BookingsWebApi/Validators/RoomValidator.cs
+++ src/BookingsWebApi/Validators/RoomValidator.cs
@@ -8,10 +8,10 @@ namespace BookingsWebApi.Validators
     {
         public RoomInsertValidator()
         {
-            RuleFor(r => r.HotelId).NotEmpty().MinimumLength(1);
             RuleFor(r => r.Name).NotEmpty().Length(2, 25);
             RuleFor(r => r.Capacity).NotEmpty().GreaterThan(0);
             RuleFor(r => r.Image).NotEmpty().MinimumLength(1);
+            RuleFor(r => r.HotelId).NotEmpty().MinimumLength(1);
         }
     }
 }
