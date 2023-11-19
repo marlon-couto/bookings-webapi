a0894636a69742dc52b4e12f3def3d7aa816f85b
Marlon Couto <coutoozair@gmail.com>
2023-11-18 23:58:06 -0300

    feat: format AutoMapperProfile

--- src/BookingsWebApi/Configuration/AutoMapperProfile.cs
+++ src/BookingsWebApi/Configuration/AutoMapperProfile.cs
@@ -9,31 +9,31 @@ namespace BookingsWebApi
     {
         public AutoMapperProfile()
         {
-            CreateMap<User, UserDto>();
             CreateMap<UserInsertDto, User>();
-            CreateMap<City, CityDto>();
+            CreateMap<User, UserDto>();
+
             CreateMap<CityInsertDto, City>();
+            CreateMap<City, CityDto>();
+
+            CreateMap<HotelInsertDto, Hotel>();
             CreateMap<Hotel, HotelDto>()
                 .ForMember(dest => dest.CityName,
                     opts => opts.MapFrom(src => src.City!.Name))
                 .ForMember(dest => dest.CityState,
                     opts => opts.MapFrom(src => src.City!.State));
 
-            CreateMap<HotelInsertDto, Hotel>();
-
+            CreateMap<RoomInsertDto, Room>();
             CreateMap<Room, RoomDto>()
                 .ForMember(dest => dest.Hotel, opts => CreateMap<Hotel, HotelDto>());
-
-            CreateMap<RoomInsertDto, Room>();
-
-            CreateMap<Booking, BookingDto>()
-                .ForMember(dest => dest.Room, opts => CreateMap<Room, RoomDto>());
 
             CreateMap<BookingInsertDto, Booking>()
                 .ForMember(dest => dest.CheckIn,
                     opts => opts.MapFrom(src => DateTime.Parse(src.CheckIn).ToUniversalTime()))
                 .ForMember(dest => dest.CheckOut,
                     opts => opts.MapFrom(src => DateTime.Parse(src.CheckOut).ToUniversalTime()));
+
+            CreateMap<Booking, BookingDto>()
+                .ForMember(dest => dest.Room, opts => CreateMap<Room, RoomDto>());
         }
     }
 }
