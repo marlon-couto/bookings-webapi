3a94e8ac1c8f2822ce083b2d2ddbc3bfd1342412
Marlon Couto <coutoozair@gmail.com>
2023-11-24 17:33:31 -0300

    feat: add abstract class Entity

---
+++ src/BookingsWebApi/Models/Entity.cs
@@ -0,0 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BookingsWebApi.Models;
+
+public abstract class Entity
+{
+    [Required]
+    public string Id { get; set; } = string.Empty;
+}
