7ed796c7e17e7e57cd06ba8269124c39e9911f31
Marlon Couto <coutoozair@gmail.com>
2023-11-29 16:33:09 -0300

    feat: add TestUtils

---
+++ tests/BookingsWebApi.Test/Helpers/TestUtils.cs
@@ -0,0 +1,27 @@
+using System.Threading.Tasks;
+
+using BookingsWebApi.Test.Context;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace BookingsWebApi.Test.Helpers;
+
+public static class TestUtils
+{
+    // Initial context setup.
+    public static TestBookingsDbContext CreateContext()
+    {
+        DbContextOptions<TestBookingsDbContext> options = new DbContextOptionsBuilder<TestBookingsDbContext>()
+            .UseInMemoryDatabase("InMemoryDb")
+            .Options;
+        return new TestBookingsDbContext(options);
+    }
+
+    // Reset the database before the test.
+    public static async Task ClearDatabase<TEntity>(this TestBookingsDbContext context, DbSet<TEntity> dbSet)
+        where TEntity : class
+    {
+        dbSet.RemoveRange(dbSet);
+        await context.SaveChangesAsync();
+    }
+}
