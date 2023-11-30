2a6b45669b7a75d7db173cf8cdb706da7d2826a5
Marlon Couto <coutoozair@gmail.com>
2023-11-29 23:33:43 -0300

    feat: add RoomServiceTest

---
+++ tests/BookingsWebApi.Test/Services/RoomServiceTest.cs
@@ -0,0 +1,125 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Bogus;
+
+using BookingsWebApi.DTOs;
+using BookingsWebApi.Models;
+using BookingsWebApi.Services;
+using BookingsWebApi.Test.Builders;
+using BookingsWebApi.Test.Context;
+using BookingsWebApi.Test.Helpers;
+
+using FluentAssertions;
+
+using Microsoft.EntityFrameworkCore;
+
+using Xunit;
+
+namespace BookingsWebApi.Test.Services;
+
+public class RoomServiceTest : IClassFixture<TestFixture>, IDisposable
+{
+    private readonly TestDbContext _context;
+    private readonly Faker _faker = new();
+    private readonly RoomService _service;
+
+    public RoomServiceTest(TestFixture fixture)
+    {
+        _context = fixture.Context;
+        _service = new RoomService(_context);
+    }
+
+    public void Dispose()
+    {
+        _context.Database.EnsureDeleted();
+        GC.SuppressFinalize(this);
+    }
+
+    [Fact(DisplayName = "AddRoom should add room")]
+    public async Task AddRoom_ShouldAddRoom()
+    {
+        Hotel hotel = HotelBuilder.New().Build();
+        RoomInsertDto dto =
+            new()
+            {
+                Name = _faker.Lorem.Sentence(),
+                HotelId = hotel.Id,
+                Capacity = _faker.Random.Int(),
+                Image = _faker.Image.PicsumUrl()
+            };
+        Room createdRoom = await _service.AddRoom(dto, hotel);
+
+        createdRoom.Should().NotBeNull();
+    }
+
+    [Fact(DisplayName = "DeleteRoom should delete room")]
+    public async Task DeleteRoom_ShouldDeleteRoom()
+    {
+        Room room = RoomBuilder.New().Build();
+        await _context.Rooms.AddAsync(room);
+        await _context.SaveChangesAsync();
+
+        await _service.DeleteRoom(room);
+
+        List<Room> allRooms = await _context.Rooms.AsNoTracking().ToListAsync();
+        allRooms.Count.Should().Be(0);
+    }
+
+    [Fact(DisplayName = "GetAllRooms should return all rooms")]
+    public async Task GetAllRooms_ShouldReturnAllUsers()
+    {
+        Room room1 = RoomBuilder.New().Build();
+        Room room2 = RoomBuilder.New().Build();
+        await _context.Rooms.AddAsync(room1);
+        await _context.Rooms.AddAsync(room2);
+        await _context.SaveChangesAsync();
+
+        List<Room> allRooms = await _service.GetAllRooms();
+
+        allRooms.Count.Should().Be(2);
+    }
+
+    [Fact(DisplayName = "GetHotelById should return hotel found")]
+    public async Task GetHotelById_ShouldReturnHotelFound()
+    {
+        Hotel hotel = HotelBuilder.New().Build();
+        await _context.Hotels.AddAsync(hotel);
+        await _context.SaveChangesAsync();
+
+        Hotel hotelFound = await _service.GetHotelById(hotel.Id);
+
+        hotelFound.Should().NotBeNull();
+    }
+
+    [Fact(DisplayName = "GetHotelById throw KeyNotFoundException if hotel not exists")]
+    public async Task GetHotelById_ThrowKeyNotFoundException_IfHotelNotExists()
+    {
+        Func<Task> act = async () => await _service.GetHotelById(_faker.Random.Guid().ToString());
+
+        await act.Should().ThrowAsync<KeyNotFoundException>()
+            .WithMessage("The hotel with the provided id does not exist.");
+    }
+
+    [Fact(DisplayName = "GetRoomById should return room found")]
+    public async Task GetRoomById_ShouldReturnRoomFound()
+    {
+        Room room = RoomBuilder.New().Build();
+        await _context.Rooms.AddAsync(room);
+        await _context.SaveChangesAsync();
+
+        Room roomFound = await _service.GetRoomById(room.Id);
+
+        roomFound.Should().NotBeNull();
+    }
+
+    [Fact(DisplayName = "GetRoomById throw KeyNotFoundException if room not exists")]
+    public async Task GetRoomById_ThrowKeyNotFoundException_IfRoomNotExists()
+    {
+        Func<Task> act = async () => await _service.GetRoomById(_faker.Random.Guid().ToString());
+
+        await act.Should().ThrowAsync<KeyNotFoundException>()
+            .WithMessage("The room with the provided id does not exist.");
+    }
+}
