f960c8f51d9d3b0949bef88acc3c50f608250c3d
Marlon Couto <coutoozair@gmail.com>
2023-11-29 23:33:33 -0300

    feat: add TokenServiceTest

---
+++ tests/BookingsWebApi.Test/Services/TokenServiceTest.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using BookingsWebApi.Models;
+using BookingsWebApi.Services;
+using BookingsWebApi.Test.Builders;
+using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using Xunit;
+
+namespace BookingsWebApi.Test.Services;
+
+public class TokenServiceTest
+{
+    private readonly TokenService _service;
+
+    public TokenServiceTest()
+    {
+        IConfiguration configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(
+                new Dictionary<string, string>
+                {
+                    {
+                        "Token:Secret",
+                        "6ce1a0e05b576372b1fc569425a1e0f5e72adad7b318bb6420a6c307b6f2ca41"
+                    },
+                    { "Token:ExpiresDay", "1" }
+                }
+            )
+            .Build();
+
+        _service = new TokenService(configuration);
+    }
+
+    [Fact(DisplayName = "Generate should generate token")]
+    public void Generate_ShouldGenerateToken()
+    {
+        User user = UserBuilder.New().Build();
+        string token = _service.Generate(user);
+
+        token.Should().NotBeNull();
+    }
+}
