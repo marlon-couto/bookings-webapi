54b321051ba05bb1118329b497ba39b565ac6c01
Marlon Couto <coutoozair@gmail.com>
2023-12-01 17:19:51 -0300

    feat: update RoomValidatorTest

--- tests/BookingsWebApi.Test/Unit/Validators/RoomValidatorTest.cs
+++ tests/BookingsWebApi.Test/Unit/Validators/RoomValidatorTest.cs
@@ -19,4 +19,43 @@ public class RoomValidatorTest
         TestValidationResult<RoomInsertDto>? result = _validator.TestValidate(dto);
         result.ShouldNotHaveAnyValidationErrors();
     }
+
+    [Theory(DisplayName = "RoomValidator should have error when name is invalid")]
+    [InlineData("")]
+    [InlineData("x")]
+    [InlineData("Lorem ipsum dolor sit amet consectetur adipiscing elit.")]
+    public void RoomValidator_ShouldHaveError_WhenNameIsInvalid(string name)
+    {
+        RoomInsertDto dto = RoomBuilder.New().WithName(name).BuildAsInsertDto();
+        TestValidationResult<RoomInsertDto>? result = _validator.TestValidate(dto);
+        result.ShouldHaveValidationErrorFor(r => r.Name);
+    }
+
+    [Theory(DisplayName = "RoomValidator should have error when hotel ID is invalid")]
+    [InlineData("")]
+    public void RoomValidator_ShouldHaveError_WhenHotelIdIsInvalid(string hotelId)
+    {
+        RoomInsertDto dto = RoomBuilder.New().WithHotelId(hotelId).BuildAsInsertDto();
+        TestValidationResult<RoomInsertDto>? result = _validator.TestValidate(dto);
+        result.ShouldHaveValidationErrorFor(r => r.HotelId);
+    }
+
+    [Theory(DisplayName = "RoomValidator should have error when image is invalid")]
+    [InlineData("")]
+    public void RoomValidator_ShouldHaveError_WhenImageIsInvalid(string image)
+    {
+        RoomInsertDto dto = RoomBuilder.New().WithImage(image).BuildAsInsertDto();
+        TestValidationResult<RoomInsertDto>? result = _validator.TestValidate(dto);
+        result.ShouldHaveValidationErrorFor(r => r.Image);
+    }
+
+    [Theory(DisplayName = "RoomValidator should have error when capacity is invalid")]
+    [InlineData(0)]
+    [InlineData(-1)]
+    public void RoomValidator_ShouldHaveError_WhenCapacityIsInvalid(int capacity)
+    {
+        RoomInsertDto dto = RoomBuilder.New().WithCapacity(capacity).BuildAsInsertDto();
+        TestValidationResult<RoomInsertDto>? result = _validator.TestValidate(dto);
+        result.ShouldHaveValidationErrorFor(r => r.Capacity);
+    }
 }
