f351e4a49442af846601b628ec680bb5fc88fce0
Marlon Couto <coutoozair@gmail.com>
2023-12-01 14:43:09 -0300

    feat: add BookingBuilder

---
+++ tests/BookingsWebApi.Test/Helpers/Builders/BookingBuilder.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Globalization;
+
+using Bogus;
+
+using BookingsWebApi.DTOs;
+using BookingsWebApi.Models;
+
+namespace BookingsWebApi.Test.Helpers.Builders;
+
+public class BookingBuilder
+{
+    private readonly DateTime _checkIn;
+    private readonly DateTime _checkOut;
+    private readonly string _id;
+    private readonly Room _room;
+    private string _checkInStr;
+    private string _checkOutStr;
+    private int _guestQuantity;
+    private string _roomId;
+    private User _user;
+
+    private BookingBuilder()
+    {
+        Faker faker = new();
+        _user = UserBuilder.New().Build();
+        _guestQuantity = faker.Random.Int(1);
+        _checkIn = DateTime.Now;
+        _checkInStr = _checkIn.ToString(CultureInfo.InvariantCulture);
+        _checkOut = DateTime.Now.AddDays(1);
+        _checkOutStr = _checkOut.ToString(CultureInfo.InvariantCulture);
+        _room = RoomBuilder.New().Build();
+        _roomId = _room.Id;
+        _id = faker.Random.Guid().ToString();
+    }
+
+    public static BookingBuilder New()
+    {
+        return new BookingBuilder();
+    }
+
+    public BookingBuilder WithUser(User user)
+    {
+        _user = user;
+        return this;
+    }
+
+    public BookingBuilder WithCheckIn(string checkInStr)
+    {
+        _checkInStr = checkInStr;
+        return this;
+    }
+
+    public BookingBuilder WithCheckOut(string checkOutStr)
+    {
+        _checkOutStr = checkOutStr;
+        return this;
+    }
+
+    public BookingBuilder WithGuestQuantity(int guestQuantity)
+    {
+        _guestQuantity = guestQuantity;
+        return this;
+    }
+
+    public BookingBuilder WithRoomId(string roomId)
+    {
+        _roomId = roomId;
+        return this;
+    }
+
+    public Booking Build()
+    {
+        return new Booking
+        {
+            CheckIn = _checkIn,
+            CheckOut = _checkOut,
+            GuestQuantity = _guestQuantity,
+            Id = _id,
+            Room = _room,
+            User = _user,
+            RoomId = _room.Id,
+            UserId = _user.Id
+        };
+    }
+
+    public BookingInsertDto BuildAsInsertDto()
+    {
+        return new BookingInsertDto
+        {
+            CheckIn = _checkInStr, CheckOut = _checkOutStr, GuestQuantity = _guestQuantity, RoomId = _roomId
+        };
+    }
+}
