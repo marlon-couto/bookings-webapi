e60e9a0ad274ccf85899c8a2069becb0ad251bfe
Marlon Couto <coutoozair@gmail.com>
2023-12-02 13:32:04 -0300

    feat: update UserBuilder

--- tests/BookingsWebApi.Test/Helpers/Builders/UserBuilder.cs
+++ tests/BookingsWebApi.Test/Helpers/Builders/UserBuilder.cs
@@ -1,22 +1,23 @@
 using Bogus;
 
+using BookingsWebApi.DTOs;
 using BookingsWebApi.Models;
 
 namespace BookingsWebApi.Test.Helpers.Builders;
 
 public class UserBuilder
 {
-    private readonly string _email;
     private readonly string _id;
-    private readonly string _name;
-    private readonly string _password;
     private readonly string _role;
+    private string _email;
+    private string _name;
+    private string _password;
 
     private UserBuilder()
     {
         Faker faker = new();
         _email = faker.Internet.Email();
-        _password = faker.Internet.Password();
+        _password = faker.Internet.Password(prefix: "Pass@12", length: 10);
         _role = "Client";
         _name = faker.Name.FirstName();
         _id = faker.Random.Guid().ToString();
@@ -25,6 +26,24 @@ public class UserBuilder
     public static UserBuilder New()
     {
         return new UserBuilder();
+    }
+
+    public UserBuilder WithName(string name)
+    {
+        _name = name;
+        return this;
+    }
+
+    public UserBuilder WithPassword(string password)
+    {
+        _password = password;
+        return this;
+    }
+
+    public UserBuilder WithEmail(string email)
+    {
+        _email = email;
+        return this;
     }
 
     public User Build()
@@ -37,5 +56,15 @@ public class UserBuilder
             Id = _id,
             Name = _name
         };
+    }
+
+    public UserInsertDto BuildAsInsertDto()
+    {
+        return new UserInsertDto { Email = _email, Name = _name, Password = _password };
+    }
+
+    public LoginInsertDto BuildAsLoginDto()
+    {
+        return new LoginInsertDto { Email = _email, Password = _password };
     }
 }
