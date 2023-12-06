e690c8e08b607b0c0f87d9ee4bce22de30b8f112
Marlon Couto <coutoozair@gmail.com>
2023-12-06 16:46:11 -0300

    feat: add Extensions.cs

---
+++ tests/BookingsWebApi.Test/Helpers/Extensions.cs
@@ -0,0 +1,12 @@
+using BookingsWebApi.DTOs;
+using BookingsWebApi.Models;
+
+namespace BookingsWebApi.Test.Helpers;
+
+public static class Extensions
+{
+    public static UserDto AsDto(this User user)
+    {
+        return new UserDto { Id = user.Id, Email = user.Email, Name = user.Name, Role = user.Role };
+    }
+}
