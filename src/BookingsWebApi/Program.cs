c515f65cb3b7b41513c7480dc5d65f150ff1dd98
Marlon Couto <ozair.couto@trieduc.com.br>
2024-08-08 13:14:30 -0300

    Melhorias na legibilidade e adição de middleware de erros
    - Adição da importação do namespace `BookingsWebApi.Middlewares`.
    - Substituição de tipos explícitos por `var` para reduzir repetição.
    - Remoção de linhas em branco desnecessárias.
    - Refatoração da configuração do Swagger para usar variável `apiInfo`.
    - Refatoração da configuração do JWT para usar variável `securityKey`.
    - Adição do middleware `ErrorHandlerMiddleware` ao pipeline HTTP.

--- src/BookingsWebApi/Program.cs
+++ src/BookingsWebApi/Program.cs
@@ -1,29 +1,25 @@
 using System.Reflection;
 using System.Security.Claims;
 using System.Text;
-
 using BookingsWebApi.Context;
 using BookingsWebApi.DTOs;
 using BookingsWebApi.Helpers;
+using BookingsWebApi.Middlewares;
 using BookingsWebApi.Models;
 using BookingsWebApi.Services;
 using BookingsWebApi.Validators;
-
 using FluentValidation;
-
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddDbContext<BookingsDbContext>(); // Allows the context to be used by EF Core.
 
 // Adds scopes for dependency injection.
-
 builder.Services.AddScoped<IBookingsDbContext, BookingsDbContext>();
 builder.Services.AddScoped<IAuthHelper, AuthHelper>();
 builder.Services.AddScoped<IBookingService, BookingService>();
@@ -33,7 +29,6 @@ builder.Services.AddScoped<IHotelService, HotelService>();
 builder.Services.AddScoped<IRoomService, RoomService>();
 
 // Request body validations.
-
 ValidatorOptions.Global.LanguageManager.Enabled = false; // Disables the translation of error messages.
 builder.Services.AddScoped<IValidator<BookingInsertDto>, BookingValidator>();
 builder.Services.AddScoped<IValidator<UserInsertDto>, UserValidator>();
@@ -44,27 +39,23 @@ builder.Services.AddScoped<IValidator<LoginInsertDto>, LoginValidator>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+var apiInfo = new OpenApiInfo
+{
+    Version = "v1", Title = "Bookings.net API", Description = "An API for managing bookings, rooms and hotels."
+};
 builder.Services.AddSwaggerGen(opts =>
 {
-    opts.SwaggerDoc(
-        "v1",
-        new OpenApiInfo
-        {
-            Version = "v1",
-            Title = "Bookings.net API",
-            Description = "An API for managing bookings, rooms and hotels."
-        }
-    );
+    opts.SwaggerDoc("v1", apiInfo);
 
     // Reflection is used to build an XML file name matching that of the web API project.
-    string xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
     opts.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
 });
-
 builder.Services.AddAutoMapper(typeof(Program).Assembly); // Configures AutoMapper in the application.
 
 // Configures JWT.
-IConfigurationSection tokenOptions = builder.Configuration.GetSection(TokenModel.Token);
+var tokenOptions = builder.Configuration.GetSection(TokenModel.Token);
+var securityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(tokenOptions.GetValue<string>("Secret")));
 builder
     .Services.AddAuthentication(opts =>
     {
@@ -80,9 +71,7 @@ builder
             ValidateIssuer = false,
             ValidateAudience = false,
             ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(
-                Encoding.ASCII.GetBytes(tokenOptions.GetValue<string>("Secret"))
-            )
+            IssuerSigningKey = securityKey
         };
     });
 
@@ -95,8 +84,7 @@ builder.Services.AddAuthorization(options =>
         policy => policy.RequireClaim(ClaimTypes.Email).RequireClaim(ClaimTypes.Role, "Admin")
     );
 });
-
-WebApplication app = builder.Build();
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -105,11 +93,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseMiddleware<ErrorHandlerMiddleware>();
 app.UseHttpsRedirection();
-
 app.UseAuthentication(); // Enables authentication middleware.
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
