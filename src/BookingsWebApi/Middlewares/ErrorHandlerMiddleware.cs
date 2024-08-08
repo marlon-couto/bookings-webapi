9b92586edf8ef5d25dea709082bf5f248e55b002
Marlon Couto <ozair.couto@trieduc.com.br>
2024-08-08 13:13:01 -0300

    Adiciona ErrorHandlerMiddleware ao BookingsWebApi
    Foi adicionado um novo middleware chamado `ErrorHandlerMiddleware` no projeto `BookingsWebApi`. Este middleware captura exceções específicas e retorna respostas HTTP apropriadas com mensagens de erro detalhadas. As exceções tratadas incluem `InvalidDateException`, `InvalidEmailException`, `InvalidInputDataException`, `MaximumCapacityException`, `NotFoundException`, `UnauthorizedException`, `UnauthorizedAccessException` e uma exceção genérica `Exception`. Dependendo da exceção capturada, o middleware define o código de status HTTP correspondente (como 400, 401, 404 ou 500) e retorna uma resposta JSON com o resultado e a mensagem de erro.

---
+++ src/BookingsWebApi/Middlewares/ErrorHandlerMiddleware.cs
@@ -0,0 +1,70 @@
+﻿using BookingsWebApi.Exceptions;
+using BookingsWebApi.Helpers;
+
+namespace BookingsWebApi.Middlewares;
+
+public class ErrorHandlerMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ErrorHandlerMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext ctx)
+    {
+        try
+        {
+            await _next(ctx);
+        }
+        catch (InvalidDateException e)
+        {
+            ctx.Response.StatusCode = StatusCodes.Status400BadRequest;
+            var res = new ControllerResponse { Result = EResult.Failed, Message = e.Message };
+            await ctx.Response.WriteAsJsonAsync(res);
+        }
+        catch (InvalidEmailException e)
+        {
+            ctx.Response.StatusCode = StatusCodes.Status400BadRequest;
+            var res = new ControllerResponse { Result = EResult.Failed, Message = e.Message };
+            await ctx.Response.WriteAsJsonAsync(res);
+        }
+        catch (InvalidInputDataException e)
+        {
+            ctx.Response.StatusCode = StatusCodes.Status400BadRequest;
+            var res = new ControllerResponse { Result = EResult.Failed, Message = e.Message };
+            await ctx.Response.WriteAsJsonAsync(res);
+        }
+        catch (MaximumCapacityException e)
+        {
+            ctx.Response.StatusCode = StatusCodes.Status400BadRequest;
+            var res = new ControllerResponse { Result = EResult.Failed, Message = e.Message };
+            await ctx.Response.WriteAsJsonAsync(res);
+        }
+        catch (NotFoundException e)
+        {
+            ctx.Response.StatusCode = StatusCodes.Status404NotFound;
+            var res = new ControllerResponse { Result = EResult.Failed, Message = e.Message };
+            await ctx.Response.WriteAsJsonAsync(res);
+        }
+        catch (UnauthorizedException e)
+        {
+            ctx.Response.StatusCode = StatusCodes.Status401Unauthorized;
+            var res = new ControllerResponse { Result = EResult.Failed, Message = e.Message };
+            await ctx.Response.WriteAsJsonAsync(res);
+        }
+        catch (UnauthorizedAccessException)
+        {
+            ctx.Response.StatusCode = StatusCodes.Status401Unauthorized;
+            var res = new ControllerResponse { Result = EResult.Failed, Message = "You must authorize first." };
+            await ctx.Response.WriteAsJsonAsync(res);
+        }
+        catch (Exception)
+        {
+            ctx.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            var res = new ControllerResponse { Result = EResult.Failed, Message = "An error occurred." };
+            await ctx.Response.WriteAsJsonAsync(res);
+        }
+    }
+}
