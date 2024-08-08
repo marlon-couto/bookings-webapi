52ffe1afd963ceaee6488aa8364f2c1adbfaa2e5
Marlon Couto <ozair.couto@trieduc.com.br>
2024-08-08 13:13:44 -0300

    Refatoração de validadores e ajuste em IsValidDate
    Remoção de linhas em branco entre as instruções `using` nos arquivos `BookingValidator.cs`, `CityValidator.cs`, `HotelValidator.cs`, `RoomValidator.cs` e `UserValidator.cs` para melhorar a organização e a consistência do código.
    
    Modificação na classe `BookingValidator` para permitir que o parâmetro `dateString` do método `IsValidDate` seja nulo, alterando a assinatura do método de `private static bool IsValidDate(string dateString)` para `private static bool IsValidDate(string? dateString)`, melhorando a robustez do código.

--- src/BookingsWebApi/Validators/BookingValidator.cs
+++ src/BookingsWebApi/Validators/BookingValidator.cs
@@ -1,7 +1,5 @@
 using System.Globalization;
-
 using BookingsWebApi.DTOs;
-
 using FluentValidation;
 
 namespace BookingsWebApi.Validators;
@@ -14,17 +12,15 @@ public class BookingValidator : AbstractValidator<BookingInsertDto>
             .NotEmpty()
             .Must(IsValidDate)
             .WithMessage("'Check In' must be a valid date.");
-
         RuleFor(b => b.CheckOut)
             .NotEmpty()
             .Must(IsValidDate)
             .WithMessage("'Check Out' must be a valid date.");
-
         RuleFor(b => b.GuestQuantity).NotEmpty().GreaterThan(0);
         RuleFor(b => b.RoomId).NotEmpty().MinimumLength(1);
     }
 
-    private static bool IsValidDate(string dateString)
+    private static bool IsValidDate(string? dateString)
     {
         return DateTime.TryParseExact(
             dateString,
