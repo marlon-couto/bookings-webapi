58a0f6f4c2db94c7981f2579f9e219cf7cc2f7c6
Marlon Couto <ozair.couto@trieduc.com.br>
2024-08-09 19:17:24 -0300

    Atualiza string de conexão e adiciona migração inicial
    - Atualiza a string de conexão no `appsettings.Development.json` para um banco de dados PostgreSQL no Azure.
    - Adiciona a migração `20240809221611_InitialCreate.cs` para criar tabelas `Cities`, `Users`, `Hotels`, `Rooms` e `Bookings` e inserir dados iniciais.
    - Adiciona o arquivo `20240809221611_InitialCreate.Designer.cs` para definir o modelo de dados e relações entre tabelas.
    - Adiciona o arquivo `BookingsDbContextModelSnapshot.cs` para capturar o estado atual do modelo de dados do contexto `BookingsDbContext`.
    - Inclui namespaces necessários e configurações de anotações e extensões.
    - Define entidades e relacionamentos, incluindo dados iniciais e navegações.

---
+++ src/BookingsWebApi/Migrations/20240809221611_InitialCreate.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookingsWebApi.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cities",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "character varying(16)", maxLength: 16, nullable: false),
+                    Name = table.Column<string>(type: "character varying(25)", maxLength: 25, nullable: false),
+                    State = table.Column<string>(type: "character varying(25)", maxLength: 25, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cities", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "character varying(16)", maxLength: 16, nullable: false),
+                    Name = table.Column<string>(type: "character varying(25)", maxLength: 25, nullable: false),
+                    Email = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Password = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Salt = table.Column<string>(type: "character varying(25)", maxLength: 25, nullable: false),
+                    Role = table.Column<string>(type: "character varying(25)", maxLength: 25, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Hotels",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "character varying(16)", maxLength: 16, nullable: false),
+                    Name = table.Column<string>(type: "character varying(25)", maxLength: 25, nullable: false),
+                    Address = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    CityId = table.Column<string>(type: "character varying(16)", maxLength: 16, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hotels", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Hotels_Cities_CityId",
+                        column: x => x.CityId,
+                        principalTable: "Cities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rooms",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "character varying(16)", maxLength: 16, nullable: false),
+                    Name = table.Column<string>(type: "character varying(25)", maxLength: 25, nullable: false),
+                    Capacity = table.Column<int>(type: "integer", nullable: false),
+                    Image = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    HotelId = table.Column<string>(type: "character varying(16)", maxLength: 16, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rooms", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Rooms_Hotels_HotelId",
+                        column: x => x.HotelId,
+                        principalTable: "Hotels",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Bookings",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "character varying(16)", maxLength: 16, nullable: false),
+                    CheckIn = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CheckOut = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    GuestQuantity = table.Column<int>(type: "integer", nullable: false),
+                    UserId = table.Column<string>(type: "character varying(16)", maxLength: 16, nullable: false),
+                    RoomId = table.Column<string>(type: "character varying(16)", maxLength: 16, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bookings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Bookings_Rooms_RoomId",
+                        column: x => x.RoomId,
+                        principalTable: "Rooms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Bookings_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "Id", "Name", "State" },
+                values: new object[,]
+                {
+                    { "1", "City 1", "State 1" },
+                    { "2", "City 2", "State 2" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "Name", "Password", "Role", "Salt" },
+                values: new object[,]
+                {
+                    { "1", "user1@mail.com", "User 1", "Of5n7kc14AwibmiFXk5ZnyDbioxlCiqxcZr7ayc/ad4=", "Client", "RDsG9pM5F0tj6ZIU2UogSA==" },
+                    { "2", "user2@mail.com", "User 2", "7eyPpIQlkJGpp/lfFJuSeJ3IZbo5DznSu/C1VG7JO9I=", "Client", "Gqude0JZ38YVvz/RUTeZ9w==" },
+                    { "3", "user3@mail.com", "User 3", "sT+OPanSmual937atM35GfT2Xp1w5yPXpTBA4U8JdEo=", "Admin", "pWIyxOGXD5363rNbX0ASfg==" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "Address", "CityId", "Name" },
+                values: new object[,]
+                {
+                    { "1", "Address 1", "1", "Hotel 1" },
+                    { "2", "Address 2", "2", "Hotel 2" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Rooms",
+                columns: new[] { "Id", "Capacity", "HotelId", "Image", "Name" },
+                values: new object[,]
+                {
+                    { "1", 2, "1", "Image 1", "Room 1" },
+                    { "2", 1, "1", "Image 2", "Room 2" },
+                    { "3", 3, "1", "Image 3", "Room 3" },
+                    { "4", 2, "2", "Image 4", "Room 4" },
+                    { "5", 1, "2", "Image 5", "Room 5" },
+                    { "6", 3, "2", "Image 6", "Room 6" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Bookings",
+                columns: new[] { "Id", "CheckIn", "CheckOut", "GuestQuantity", "RoomId", "UserId" },
+                values: new object[,]
+                {
+                    { "1", new DateTime(2023, 11, 7, 0, 0, 0, 0, DateTimeKind.Utc), new DateTime(2023, 11, 8, 0, 0, 0, 0, DateTimeKind.Utc), 1, "1", "1" },
+                    { "2", new DateTime(2023, 11, 7, 0, 0, 0, 0, DateTimeKind.Utc), new DateTime(2023, 11, 8, 0, 0, 0, 0, DateTimeKind.Utc), 1, "2", "1" },
+                    { "3", new DateTime(2023, 11, 7, 0, 0, 0, 0, DateTimeKind.Utc), new DateTime(2023, 11, 8, 0, 0, 0, 0, DateTimeKind.Utc), 1, "3", "2" },
+                    { "4", new DateTime(2023, 11, 7, 0, 0, 0, 0, DateTimeKind.Utc), new DateTime(2023, 11, 8, 0, 0, 0, 0, DateTimeKind.Utc), 1, "4", "2" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_RoomId",
+                table: "Bookings",
+                column: "RoomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_UserId",
+                table: "Bookings",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Hotels_CityId",
+                table: "Hotels",
+                column: "CityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rooms_HotelId",
+                table: "Rooms",
+                column: "HotelId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bookings");
+
+            migrationBuilder.DropTable(
+                name: "Rooms");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Hotels");
+
+            migrationBuilder.DropTable(
+                name: "Cities");
+        }
+    }
+}
